cd3d557bbb4ba0b612bb7f7a6ce44078a42994b7
geraldbaeck <gerald@baeck.at>
2017-07-16 19:16:24 +0200

    new setup routine

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -r GITHUB_REPOSITORY="geraldbaeck/dotfiles"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Create the necessary symbolic links between the `.dotfiles` and `HOME`
+# directory.
+shopt -s dotglob  # include hidden files
+for f in bash/* ; do
+  ln -fs "${PWD}/${f}" "${HOME}/`basename ${f}`"  # create symlink
+  echo "  `basename ${f}` symlinked to "
+  echo "    ${PWD}/${f}"
+done
+echo "Linking done."
